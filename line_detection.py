0a643fe1945f01777d9bdf7f54379d9e08fbba5a
filchy <54070973+filchy@users.noreply.github.com>
2020-01-02 12:03:05 +0100

    Update line_detection.py

--- line_detection.py
+++ line_detection.py
@@ -6,16 +6,12 @@ import math
 
 def region_of_interest(img, peaks):
 	mask = np.zeros_like(img)
-	
 	if len(img.shape) > 2:
 		channel_count = img.shape[2]
 		ignore_mask_color = (255, ) * channel_count
-	
 	else:
 		ignore_mask_color = 255
-
 	cv2.fillPoly(mask, peaks, ignore_mask_color)
-
 	masked_img = cv2.bitwise_and(img, mask)
 	
 	return masked_img, mask
@@ -57,11 +53,9 @@ def get_lane_lines(img, all_lines, solid_lines=True):
 
 	if solid_lines == True:
 		candidate_lines = []
-
 		for line in detected_lines:
 			if 0.1 <= np.abs(line.slope) <= 4:
 				candidate_lines.append(line)
-
 		lane_lines = compute_lane_from_candidates(candidate_lines, img.shape)
 	else:
 		lane_lines = detected_lines
@@ -71,26 +65,21 @@ def get_lane_lines(img, all_lines, solid_lines=True):
 
 def process_frame(img, solid_lines=True):
 	img = cv2.resize(img, (960, 540))
-
 	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 	img_blur = cv2.GaussianBlur(img_gray, (15,15), 0)
-
 	img_canny = cv2.Canny(img_blur, 50, 80)
-
 	img_h, img_w = img_canny.shape[0], img_canny.shape[1]
-
+	
 	peaks = np.array([[(50,img_h),
-						(450,320),
-						(510,320),
-						(img_w-50,img_h)]],dtype=np.int32)
-
+			(450,320),
+			(510,320),
+			(img_w-50,img_h)]],dtype=np.int32)
+	
 	img_masked, _ = region_of_interest(img_canny, peaks)
 
 	all_lines = hough_lines_detection(img_masked, 2, np.pi/180, 10, 15)
 
 	detected_lines = get_lane_lines(img_masked, all_lines, solid_lines=True)
-
 	for line in detected_lines:
 		line.draw(img)
 	
