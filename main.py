5d6c18d3787ce01ff3ef20f732a84920fd497ab4
filchy <54070973+filchy@users.noreply.github.com>
2020-01-02 12:01:46 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from line_detection import process_frame
+
+import cv2
+
+def main():
+	cap = cv2.VideoCapture("test_video1.mp4")
+
+	while cap.isOpened():
+		ret, color_frame = cap.read()
+
+		line_frame = process_frame(color_frame, solid_lines=True)
+		
+
+		cv2.imshow("main", line_frame)
+		if cv2.waitKey(35) == 27:
+			break
+
+	cv2.destroyAllWindows()
+	cap.release()
+
+
+if __name__ == '__main__':
+	main()
